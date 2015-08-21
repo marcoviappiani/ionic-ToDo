0d9538da9e82ff2d3150e2a9e2bc8beacb37f79a
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-20 17:26:48 -0700

    Add tasks

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,12 @@
 
 angular.module('todo', ['ionic'])
 
+
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    {title: 'Collect coins'},
+    {title: 'Eat mushrooms'},
+    {title: 'Get high enough to grab the flah'},
+    {title: 'Find the Princess'}
+  ];
+});
